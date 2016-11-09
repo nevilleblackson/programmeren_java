08c6ac4fadcf602ae0c160e5ea66051342e09d16
nevilleblackson <nblackson@roc-dev.com>
2016-11-09 13:43:40 +0100

    h13

--- src/hoofdstuk_13/Praktijkopdracht.java
+++ src/hoofdstuk_13/Praktijkopdracht.java
@@ -12,37 +12,35 @@ public class Praktijkopdracht extends Applet {
     }
 
     public void paint(Graphics g) {
-        boom(g, 4);
+        boom(g, 10);
     }
 
 
     void boom(Graphics g, int aantalrijen) {
 
-        int x, x2, y, y2, x3, length;
+        int x, y, x2, y2;
+
         x = 100;
         y = 100;
-        x2 = 150;
-        length = 300;
-        y2 = 30;
-        x3 = 50;
-        //aantalrijen = aantalrijen *
+        x2 = 80;
+        y2 = 60;
+        int X = 4;
 
-        for (int i = 0; i < 10; i++) {
-            x += 200;
-            x2 += 200;
-            x3 += 200;
+        for (int i = 0; i < aantalrijen; i++) {
+            x += 100;
+            x2 += 100;
 
-            g.fillRect(50, 50, 100, 100);
-            g.drawLine(x, y, x, length);
-            g.drawLine(x2, y, x2, length);
-            g.setColor(Color.green);
-            g.fillArc(x3, y2, 150, 150, 360, 360);
             g.setColor(Color.black);
+            g.fillRect(x, y, 30, 100);
+            g.setColor(Color.green);
+            g.fillArc(x2, y2, 70, 70, 360, 360);
 
-            if (i == 5) {
+            if (i == X) {
+                y += 180;
+                y2 += 180;
                 x = 100;
-                x2 = 150;
-                x3 = 50;
+                x2 = 80;
+                X += 5;
             }
         }
     }
