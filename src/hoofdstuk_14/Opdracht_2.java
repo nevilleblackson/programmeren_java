25d19195ded3f7fe1cddec6b8022002fcb561d6c
nevilleblackson <nblackson@roc-dev.com>
2016-11-19 17:28:15 +0100

    h14 af                                                   2 moet noch zorgen dat het maar een keer voor komt

--- src/hoofdstuk_14/Opdracht_2.java
+++ src/hoofdstuk_14/Opdracht_2.java
@@ -5,44 +5,56 @@ import java.awt.*;
 
 public class Opdracht_2 extends Applet {
 
-    double r, r2;
-    int worp, worp2;
     private String[] kaart = {"ruiten een", "ruiten twee", " ruiten drie", " ruiten fier", " ruiten vijf", " ruiten zes", "ruiten zeven", " ruiten acht", "ruiten negen", "ruiten tien", " ruiten boer", "ruiten vrouw", " ruiten heer", "ruiten aas",
             "harten een", "harten twee", " harten drie", " harten fier", " harten vijf", " harten zes", "harten zeven", " harten acht", "harten negen", "harten tien", " harten boer", "harten vrouw", " harten heer", "harten aas",
             "klaveren een", "klaveren twee", " klaveren drie", " klaveren fier", " klaveren vijf", " klaveren zes", "klaveren zeven", " klaveren acht", "klaveren negen", "klaveren tien", " klaveren boer", "klaveren vrouw", " klaveren heer", "klaveren aas",
             "schoppen een", "schoppen twee", " schoppen drie", " schoppen fier", " schoppen vijf", " schoppen zes", "schoppen zeven", " schoppen acht", "schoppen negen", "schoppen tien", " schoppen boer", "schoppen vrouw", " schoppen heer", "schoppen aas"};
-    String[] p1, p2, p3, p4;
+    private String[] p1, p2, p3, p4;
 
     public void init() {
         p1 = new String[13];
         p2 = new String[13];
         p3 = new String[13];
         p4 = new String[13];
+
+        kaarten(p1);
+        kaarten(p2);
+        kaarten(p3);
+        kaarten(p4);
     }
 
     public void paint(Graphics g) {
 
         for (int i = 0; i < p1.length; i++) {
-
-            r = Math.random();
-            worp = (int) (r * kaart.length);
-            p1[i] = kaart[worp];
-            g.drawString(p1[i], 100, 50 * i + 20);
-
-            r = Math.random();
-            worp = (int) (r * kaart.length);
-            p2[i] = kaart[worp];
-            g.drawString(p2[i], 300, 50 * i + 20);
-
-            r = Math.random();
-            worp = (int) (r * kaart.length);
-            p3[i] = kaart[worp];
-            g.drawString(p3[i], 500, 50 * i + 20);
-
-            r = Math.random();
-            worp = (int) (r * kaart.length);
-            p4[i] = kaart[worp];
-            g.drawString(p4[i], 700, 50 * i + 20);
+            g.drawString(p1[i],50,50 * i + 10);
+            g.drawString(p2[i],250,50 * i + 10);
+            g.drawString(p3[i],450,50 * i + 10);
+            g.drawString(p4[i],650,50 * i + 10);
         }
+    }
+
+    private String[] kaarten(String[] k) {
+        int schud;
+
+        for (int i = 0; i < k.length ; i++) {
+            double r = Math.random();
+            schud = (int) (r * kaart.length);
+            k[i] = kaart[schud];
+
+            String[] deck = new String[kaart.length - 1];
+
+            int j2 = 0;
+            for (int j = 0; j < deck.length ; j++) {
+
+                if(j2 == schud){
+                    j2 += 1;
+                }
+                deck[j] = kaart[j2];
+                j2++;
+            }
+            kaart = deck;
+
+        }
+        return k;
     }
 }
--- src/hoofdstuk_14/Praktijkopdracht.java
+++ src/hoofdstuk_14/Praktijkopdracht.java
@@ -4,14 +4,17 @@ import java.awt.*;
 import java.applet.Applet;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 
 public class Praktijkopdracht extends Applet {
 
+    private boolean zet = true;
     private TextField spelText;
     private int aantal = 23;
-    private int spelTexta;
+    private int spelerzet;
     private int compzet;
     private String win;
+    private Image afbeelding;
 
     public void init() {
         spelText = new TextField("",10);
@@ -19,68 +22,97 @@ public class Praktijkopdracht extends Applet {
         spelText.addActionListener(new spelTextListener());
 
         add(spelText);
+
+        URL pad = Praktijkopdracht.class.getResource("/images/");
+        afbeelding = getImage(pad, "heart symbol.png");
     }
 
     public void paint(Graphics g) {
-        if (aantal > 0) {
-            g.drawString("" + aantal, 100, 100);
-            g.drawString("laatste zet speler" + spelTexta, 200, 200);
-            g.drawString("laatste zet comp" + compzet, 200, 230);
+        if(zet) {
+            if (aantal > 0) {
+
+                int x = 50;
+                int y = 100;
+
+                for (int i = 0; i < aantal; i++) {
+                    g.drawImage(afbeelding, x, y, 20, 20, this);
+
+                    x += 50;
+                    if(i % 4 == 3){
+                        x = 50;
+                        y += 50;
+                    }
+                }
+
+                g.drawString("laatste zet speler" + spelerzet, 200, 50);
+                g.drawString("laatste zet comp" + compzet, 200, 70);
+            }
+            else {
+                g.drawString(win, 100, 100);
+            }
         }
-        else {
-            g.drawString(win , 100,100);
+
+        else{
+            zet = true;
+            g.drawString("getal tussen 1 en 3",100,100);
         }
     }
 
     private int Cset() {
+        int END = 0;
 
-    int END = 0;
+        double r;
+        int r2;
 
-            double r;
-            int r2;
+        r = Math.random();
+        r2 = (int) (r * 3 + 1);
+        int c;
 
-            r = Math.random();
-            r2 = (int) (r * 3 + 1);
-            int c;
+        c = aantal % 4;
 
-            c = aantal % 4;
+        if (c == 0) {
+            END = 3;
+        }
 
-            if (c == 0) {
-                END = 3;
-            }
+        if (c == 1) {
+            END = r2;
+        }
 
-            if (c == 1) {
-                END = 2;
-            }
+        if (c == 2) {
+            END = 1;
+        }
 
-            if (c == 2) {
-                END = 1;
-            }
-
-            if (c == 3) {
-                END = r2;
-            }
-
-    return END;
+        if (c == 3) {
+            END = 2;
+        }
+        return END;
     }
 
     private class spelTextListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
 
             String a = spelText.getText();
-            spelTexta = Integer.parseInt(a);
+            spelerzet = Integer.parseInt(a);
 
-            aantal -= spelTexta;
 
-            if (aantal == 0) {
-                win = "je hebt gewonnen";
+            if(spelerzet < 4 && spelerzet > 0) {
+
+                aantal -= spelerzet;
+                if (aantal < 0) {
+                    win = "je hebt verloren";
+                } else {
+
+                    compzet = Cset();
+                    aantal -= compzet;
+                    if (aantal < 0) {
+                        win = "je hebt gewonnen";
+                    }
+
+                }
             }
 
-            compzet = Cset();
-            aantal -= compzet;
-
-            if (aantal == 0) {
-                win = "je hebt verloren";
+            else{
+                zet = false;
             }
             repaint();
         }
